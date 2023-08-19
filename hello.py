f8f66ab1d949f69a8378c5afdf0b4023dcf066f8
frohlichramon01 <frohlichramon01@gmail.com>
2023-08-19 15:46:33 -0300

    adicionado git ignore

--- hello.py
+++ hello.py
@@ -1,1 +1,19 @@
-print("Ramon")
+# SHEBANG is a "message", that starts with #! to the OS, usually to
+# tell what to use to run this script
+
+import os
+
+current_language = os.getenv("LANG")[:5]
+
+msg = "Hello World!"
+
+if current_language == "pt_BR":
+    msg = "Olá mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao mondo!"
+elif current_language == "es_SP":
+    msg = "Hola mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour monde!"
+
+print(msg)
